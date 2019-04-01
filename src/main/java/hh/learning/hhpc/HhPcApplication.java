c76b127551e3cc41a5a1dd9a194cbc282b83e76c
Hoang Nguyen <=>
2019-04-01 17:51:28 -0400

    Initialize Project Add

---
+++ src/main/java/hh/learning/hhpc/HhPcApplication.java
@@ -0,0 +1,13 @@
+package hh.learning.hhpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhPcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhPcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hh/learning/hhpc/HhPcApplicationTests.java
@@ -0,0 +1,16 @@
+package hh.learning.hhpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HhPcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
