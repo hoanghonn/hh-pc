9392493a22dd415b3c47808a8de42ef183941061
Hoang Nguyen <=>
2019-04-07 14:29:34 -0400

    Add POJO data model.

---
+++ src/main/java/hh/learning/hhpc/model/Owner.java
@@ -0,0 +1,4 @@
+package hh.learning.hhpc.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/hh/learning/hhpc/model/PetType.java
@@ -0,0 +1,14 @@
+package hh.learning.hhpc.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
