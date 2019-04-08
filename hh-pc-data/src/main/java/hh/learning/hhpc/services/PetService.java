4eb9f4580548c105eb1671fc6fe146020ed7bae5
Hoang Nguyen <=>
2019-04-08 14:03:19 -0400

    Refactor to common base interface.

--- hh-pc-data/src/main/java/hh/learning/hhpc/services/PetService.java
+++ hh-pc-data/src/main/java/hh/learning/hhpc/services/PetService.java
@@ -2,12 +2,5 @@ package hh.learning.hhpc.services;
 
 import hh.learning.hhpc.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- hh-pc-data/src/main/java/hh/learning/hhpc/services/VetService.java
+++ hh-pc-data/src/main/java/hh/learning/hhpc/services/VetService.java
@@ -2,12 +2,5 @@ package hh.learning.hhpc.services;
 
 import hh.learning.hhpc.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
