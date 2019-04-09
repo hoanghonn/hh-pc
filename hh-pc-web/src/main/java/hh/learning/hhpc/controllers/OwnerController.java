7803631516e30d1a9e76d5c00970c832a25eb51b
Hoang Nguyen <=>
2019-04-09 13:51:48 -0400

    Add Owner Page and Controller

---
+++ hh-pc-web/src/main/java/hh/learning/hhpc/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package hh.learning.hhpc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwner(){
+
+        return "owners/index";
+    }
+}
