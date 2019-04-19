473a39e5c258e656de46be3307047f508ae0ab8b
Hoang Nguyen <hoangnguyenthai97@gmail.com>
2019-04-19 16:06:29 -0400

    GET vet list

--- hh-pc-web/src/main/java/hh/learning/hhpc/controllers/VetController.java
+++ hh-pc-web/src/main/java/hh/learning/hhpc/controllers/VetController.java
@@ -1,13 +1,23 @@
 package hh.learning.hhpc.controllers;
 
+import hh.learning.hhpc.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVet(){
+    public String listVet(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
